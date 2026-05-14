a49397a6846f4007f96aceaf8c220b1a53625da5
Arif <soyluarif@gmail.com>
2026-05-14 08:31:50 +0200

    uri2

--- apps/web/src/environments/environment.prod.ts
+++ apps/web/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://https://dutchhandbook.onrender.com/api/v1',
+  apiBaseUrl: 'https://dutchhandbook.onrender.com/api/v1',
   siteUrl: 'https://moredutch.com',
 };
